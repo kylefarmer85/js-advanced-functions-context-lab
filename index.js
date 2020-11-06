c6ec02349ca1301537a077820e8e7557d71d79d1
kylefarmer85 <kylefarmer85@gmail.com>
2020-11-05 21:09:18 -0600

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,59 @@
 /* Your Code Here */
+let createEmployeeRecord = function(empArray){
+    return {
+      firstName: empArray[0],
+      familyName: empArray[1],
+      title: empArray[2],
+      payPerHour: empArray[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    }
+  }
+
+  let createEmployeeRecords = function(arrayOfEmps){
+    return arrayOfEmps.map(empArray => {
+     return createEmployeeRecord(empArray)
+    })
+  }
+
+  let createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+  
+    this.timeInEvents.push({ 
+      type: "TimeIn",
+      hour: parseInt(hour),
+      date: date
+   // hour: parseInt(dateStamp.slice(11)),
+   // date: dateStamp.slice(0,10)
+    })
+    return this
+  }
+
+  let createTimeOutEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+    this.timeOutEvents.push({ 
+      type: "TimeOut",
+      hour: parseInt(hour),
+      date: date
+    })
+    return this
+  }  
+
+  let hoursWorkedOnDate = function(date){
+    let inDate = this.timeInEvents.find(key => {
+      return key.date === date
+      })
+    let outDate = this.timeOutEvents.find(key => {
+      return key.date === date
+      })
+    return (outDate.hour - inDate.hour) / 100
+  }
+
+  let wagesEarnedOnDate = function(dateSought){
+    let rawWage = hoursWorkedOnDate.call(this, dateSought)
+        * this.payPerHour
+    return parseFloat(rawWage.toString())
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,3 +75,13 @@ let allWagesFor = function () {
 
     return payable
 }
+
+  let findEmployeeByFirstName = function(arrayOfEmps, firstName){
+    return arrayOfEmps.find(empRecObj => empRecObj.firstName === firstName)
+  }
+  
+  let calculatePayroll = function(arrayOfEmps){
+    return arrayOfEmps.reduce((memo, empRecObj) => {
+      return memo + allWagesFor.call(empRecObj)
+    }, 0)
+  }
